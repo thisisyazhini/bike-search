9b11f350ad6df580a3efa60e1bd6851fa47e5383
thisisyazhini <yazhihari3@gmail.com>
2025-03-24 22:42:44 +0100

    Add routes and required components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { BikeSearchComponent } from '../bike-search/bike-search.component';
+import { BikeDetailComponent } from '../bike-detail/bike-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: BikeSearchComponent,
+  },
+  {
+    path: ':id',
+    component: BikeDetailComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
---
+++ src/bike-detail/bike-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bike-detail',
+  templateUrl: 'bike-detail.component.html',
+})
+export class BikeDetailComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
